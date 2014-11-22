373e49153af5cc998b3d8d5ae619b8621ac69747
c2y3dc <ervinchow@gmail.com>
2014-11-21 17:17:52 -0800

    created mysql database schema

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,7 +5,10 @@ var bluebird = require('bluebird');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      res.end('hi!');
+
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,24 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+
+create table messages
+(text varchar(160),
+ user_id int(11),
+ room_id int(11),
+ RID int(11) NOT NULL auto_increment,
+ primary KEY (RID));
+
+create table users
+(username varchar(15),
+ RID int(11) NOT NULL auto_increment,
+ primary KEY (RID));
+
+create table rooms
+(roomname varchar(15),
+ RID int(11) NOT NULL auto_increment,
+ primary KEY (RID));
+
 
 /* Create other tables and define schemas for them here! */
 
